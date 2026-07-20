696d493e8e414b66306ad3af91ac7b20a97d9cdc
Asilbek1702 <asilbektashpulatov687@gmail.com>
2026-07-20 11:24:43 +0500

    Update Supabase URL in supabaseClient.js

--- site/src/supabaseClient.js
+++ site/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Вставьте вашу ссылку напрямую вместо process.env
-const supabaseUrl = 'https://ваша-ссылка-из-supabase.supabase.co'; 
+const supabaseUrl = 'https://btxkdhxwswavfdpslnsv.supabase.co/rest/v1/'; 
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
