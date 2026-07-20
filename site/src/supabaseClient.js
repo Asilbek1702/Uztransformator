49d53ffe72289ee19922c5a3dff757f1ff3ce063
Asilbek1702 <asilbektashpulatov687@gmail.com>
2026-07-20 11:27:19 +0500

    Update supabaseAnonKey to use quotes

--- site/src/supabaseClient.js
+++ site/src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Вставьте вашу ссылку напрямую вместо process.env
 const supabaseUrl = 'https://btxkdhxwswavfdpslnsv.supabase.co/rest/v1/'; 
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0eGtkaHh3c3dhdmZkcHNsbnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNjAwNDksImV4cCI6MjA5OTkzNjA0OX0.Tw-7hdQX7rkz1_a5QaAEYXkJL167fEfjsqY7PqV5Vo0;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0eGtkaHh3c3dhdmZkcHNsbnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNjAwNDksImV4cCI6MjA5OTkzNjA0OX0.Tw-7hdQX7rkz1_a5QaAEYXkJL167fEfjsqY7PqV5Vo0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
